c14c3484924bf5ff927f7fa8394391795c3b273f
TD1223 <08173137mizutani@gmail.com>
2013-06-05 19:59:20 +0900

    curl test

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-phpinfo();
-?>
+<html>
+<head></head>
+<body>
+$url = "https://www.google.co.jp/";
+//$url = "http://www.yahoo.co.jp";
+$ch = curl_init();
+curl_setopt( $ch, CURLOPT_URL, $url );
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+
+$result = curl_exec( $ch );
+curl_close($ch);
+
+echo $result;
+</body>
+</html>
