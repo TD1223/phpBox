c931d7c4ade244326ef56fed2d86627e67bb96aa
TD1223 <08173137mizutani@gmail.com>
2013-06-05 20:12:52 +0900

    bootstrap deployed

--- index.php
+++ index.php
@@ -1,17 +1,45 @@
-<html>
-<head></head>
-<body>
-<?php
-$url = "https://www.google.co.jp/";
-//$url = "http://www.yahoo.co.jp";
-$ch = curl_init();
-curl_setopt( $ch, CURLOPT_URL, $url );
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Bootstrap 101 Template</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #fff;
+      }
+    
+      
+    </style>
+</head>
+<body style="padding-top:40px">
 
-$result = curl_exec( $ch );
-curl_close($ch);
 
-echo $result;
-?>
+
+<!--navigation bar-->
+<div class="navbar">
+    <div class="container">
+    <div class="navbar-inner ">
+        <a class="brand">KagiBox</a>
+        <ul class="nav">
+            <li class="active"><a href="top.html">TOP</a></li>
+        </ul>
+    </div>
+    </div>
+</div>
+
+<!--toppage-->
+<div class="container " style="text-align: center">
+ <p>初めての利用の方</p>
+ <p><button class="bt btn-large" type="button" onClick="location.href='register.html'">新規登録</button></p>
+ <o>既に登録済みの方</p>
+ <p><button class="btn btn-large" type="button" onClick="location.href='login.html'">ログイン</button></p>
+
+</div>
+
+<script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
